b75fcf67f693ec30356a6de6528e2b422e2889d3
wenke.lai <wenke.studio@gmail.com>
2024-12-29 18:15:31 +0800

    feat: add clerk components

---
+++ src/lib/clerk/unstyled/sign-out-button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Clerk } from '@clerk/types';
+	import Button from '$lib/components/button/button.svelte';
+	import { clerk } from '../store';
+
+	const { children } = $props();
+
+	const signOut = (clerk: Clerk) => {
+		clerk.signOut();
+	};
+</script>
+
+{#if $clerk && $clerk.user}
+	<Button onclick={() => signOut($clerk)}>
+		{#if children}
+			{@render children()}
+		{:else}
+			<p>Sign out</p>
+		{/if}
+	</Button>
+{/if}
